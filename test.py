eb6b56bc975ea9841a0ea99d3fd2eaad7642c87c
rw <pjd0149@gmail.com>
2025-01-07 13:45:35 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-print('111')
 url = 'https://ridibooks.com/category/bestsellers/2200'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
-print('html: ', html)
 soup = BeautifulSoup(html, 'html.parser')
-print('soup: ', soup)
 
 bookservices = soup.select('.fig-16r7kln')
 for no, book in enumerate(bookservices, 1):
   print(no, book.text.strip())
-  print('222')

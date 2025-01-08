ca8a62d4749ee2aea2f0bcc14a640b9dd6d8daef
rw <pjd0149@gmail.com>
2025-01-08 17:23:09 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'https://ridibooks.com/category/bestsellers/2200'
+#url = 'https://ridibooks.com/category/bestsellers/2200'
+url = 'https://paullab.co.kr/stock.html'
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
 soup = BeautifulSoup(html, 'html.parser')
 
-bookservices = soup.select('.fig-16r7kln')
+bookservices = soup.select('.date')
 for no, book in enumerate(bookservices, 1):
   print(no, book.text.strip())
